a6480066fbfe9618eb76879e965abd126f45f191
Thanyani Tshikonwani <thanyanitshikonwani@umuzi.org>
2020-02-05 01:12:24 +0200

    StringCalculator class fix

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,52 +1,86 @@
 import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class StringCalculator {
 
-   static int add(String input) {
+    static int add(String input) {
 
-     String delimiter = ",|\n";
-     String[] parts =input.split("\n",2);
-     boolean startsWith =input.startsWith("//");
+        String delimiter = ",|\n";
+        String[] parts =input.split("\n");
+        boolean startsWith =input.startsWith("//");
 
-     if (input.isEmpty()) {
-         return 0;
-     }
+        if (input.isEmpty())
+            return 0;
+        try {
+            if (input.contains("\n1000;")){
+                throw new NumberFormatException();
+            }
+        }catch (NumberFormatException e){
+            System.out.println("ERROR: Invalid inputs 1000");
+        }
 
-     if (startsWith){
-        delimiter =parts[0].substring(2);
-        input = parts[1];
-     }
-
-     if (input.contains(";\n1000;")){
-         throw new NumberFormatException("Errror: Invalid inputs");
-     }
-
-     List<String> num = Arrays.asList(input.split(delimiter));
-
-
-         try {
-             if (getIntStream(num).anyMatch(n->n<0))
-                 throw new IllegalArgumentException();
-         }catch (IllegalArgumentException e){
-             System.out.println("ERROR: negatives not allowed");
-         }
+        if (startsWith){
+            delimiter =parts[0].substring(2);
+            input = parts[1];
+        }
+        customerDelimiter ( input );
 
 
-     return getIntStream(num).sum();
- }
+        List<String> num = Arrays.asList(input.split(delimiter));
+        try {
+            if (getIntStream(num).anyMatch(n->n<0))
+                throw new IllegalArgumentException();
+        }catch (IllegalArgumentException e){
+            System.out.println ("error : negatives number are not allowed");
+        }
+        return getIntStream(num).sum();
+    }
+
     private static IntStream getIntStream(List<String> num) {
         return num.stream().mapToInt(Integer::parseInt).map(n->n%1000);
     }
 
+    private static String customerDelimiter(String input) {
+        input = input.replace("\n", ",");
+        if(input.startsWith("//[")) {
+            String delim = input.substring(3, input.indexOf ( "]" ));
+            if(delim.contains("-"))
+                throw new IllegalArgumentException("Illegal delimiter: " + delim);
+            input = input.substring(5 + delim.length());
+            input = input.replace(delim, ",");
+        }
+        return input;
+    }
+
+
+
+    private static String parseDelimiter(String header) {
+
+        String delimiter = header.substring(2);
+        if (delimiter.startsWith("[")) {
+            delimiter = delimiter.substring(1, delimiter.length() - 1);
+        }
+        return Stream.of(delimiter.split("]\\["))
+                .map(Pattern::quote)
+                .collect(Collectors.joining("|"));
+    }
+
+
 
     public void inputs(){
-            Scanner scanner = new Scanner(System.in);
-            String input = scanner.nextLine();
-            System.out.println(String.valueOf(add(input)));
-        }
+
+        System.out.println ("add(\"\") = " +(add("")));
+        System.out.println ("add(\"2,3\") = " +(add("2,3")));
+        System.out.println ("add(\"1\\n2,3\") = " +(add("1\n2,3")));
+        System.out.println ("add(\"//;\\n1;2\") = " +(add("//;\n1;2")));
+        System.out.println ("add(\"//4\\n142\") = " +(add("//4\n142")));
+        System.out.println ("add(\"//4\\n142\") = " +(add(parseDelimiter ("//[:D][&]\n:D2&3"))));
+        System.out.println("add(\"//[***]\\n1***2***3\") = "+(add ( customerDelimiter ("//[***]\n1***2***3"))));
+    }
 }
 
 
