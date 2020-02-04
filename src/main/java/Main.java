94cb18e91434832dda33e7d81fef4b93cc94fe83
Thanyani Tshikonwani <thanyanitshikonwani@umuzi.org>
2020-02-05 01:13:47 +0200

    StringCalculator class fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,15 +3,8 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String rerun;
-        do {
-            StringCalculator stringCalculator = new StringCalculator();
-            System.out.println("Type in Digits");
-            stringCalculator.inputs();
-            System.out.println("To rerun please type Yes or no to quit");
-            rerun = scan.nextLine();
-        }while (rerun.equalsIgnoreCase("yes"));
 
-    }
+            StringCalculator stringCalculator = new StringCalculator();
+            stringCalculator.inputs();
+}
 }
